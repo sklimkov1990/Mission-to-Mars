c072ff4b7eb5a34b3ae9b3bdb5287b22d7388750
Serge Klimkov <sergeklimkov@Serges-MBP.hsd1.ca.comcast.net>
2020-10-15 15:12:06 -0700

    final functions defined and put into one function

--- scraping.py
+++ scraping.py
@@ -2,13 +2,28 @@
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
 import pandas as pd
+import datetime as dt
 
-# Path to chromedriver
-!which chromedriver
 
-# Set the executable path and initialize the chrome browser in splinter
-executable_path = {'executable_path': '/usr/local/bin/chromedriver'}
-browser = Browser('chrome', **executable_path)
+def scrape_all():
+    # Initiate headless driver for deployment
+    browser = Browser("chrome", executable_path="chromedriver", headless=True)
+
+    news_title, news_paragraph = mars_news(browser)
+
+    # Run all scraping functions and store results in a dictionary
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
+    }
+
+    # Stop webdriver and return data
+    browser.quit()
+    return data
+
 
 def mars_news(browser):
 
@@ -38,9 +53,6 @@ def mars_news(browser):
     return news_title, news_p
 
 
-# ## JPL Space Images Featured Image
-
-# Visit URL
 def featured_image(browser):
     # Visit URL
     url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
@@ -72,8 +84,6 @@ def featured_image(browser):
 
     return img_url
 
-# ## Mars Facts
-
 def mars_facts():
     # Add try/except for error handling
     try:
@@ -88,4 +98,9 @@ def mars_facts():
     df.set_index('Description', inplace=True)
 
     # Convert dataframe into HTML format, add bootstrap
-    return df.to_html()
+    return df.to_html(classes="table table-striped")
+
+if __name__ == "__main__":
+
+    # If running as script, print scraped data
+    print(scrape_all())
